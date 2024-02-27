fffc9de4dfd905114e476351b4667830a913602a
sarah <sarah.emmoth@proton.me>
2024-02-27 13:15:42 +0100

    aos settings edited delay. added linkedin logo icon

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -78,10 +78,10 @@ AOS.init({
   useClassNames: true, // if true, will add content of `data-aos` as classes on scroll
   disableMutationObserver: false, // disables automatic mutations' detections (advanced)
   debounceDelay: 50, // the delay on debounce used while resizing window (advanced)
-  throttleDelay: 99, // the delay on throttle used while scrolling the page (advanced)
+  throttleDelay: 70, // the delay on throttle used while scrolling the page (advanced)
 
   // Settings that can be overridden on per-element basis, by `data-aos-*` attributes:
-  offset: 120, // offset (in px) from the original trigger point
+  offset: 20, // offset (in px) from the original trigger point
   delay: 0, // values from 0 to 3000, with step 50ms
   duration: 1000, // values from 0 to 3000, with step 50ms
   easing: "ease", // default easing for AOS animations
