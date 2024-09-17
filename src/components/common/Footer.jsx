236a02affcfd200d982b4ea925e9fc562bae011f
Sarah <sarah.emmoth@gmail.com>
2024-09-17 17:45:15 +0200

    improved footer text clarity

--- src/components/common/Footer.jsx
+++ src/components/common/Footer.jsx
@@ -5,10 +5,10 @@ const Footer = () => {
     <section className="m-0 mt-14 container-lg mx-auto p-0 flex-column flex">
       <footer className="border-bottom-0 relative rounded-t w-full px-4 lg:px-0">
           <div className="contact">
-            <h2 style={{fontSize: '2.5rem'}} className="headline">Want to chat?</h2>
+            <h2 style={{fontSize: '2.5rem'}} className="headline text-blur w-fit">Want to chat?</h2>
             <div className="flex flex-row justify-content-between">
             <a
-              className="cursive chat flex flex-row items-center gap-1 hover:underline"
+              className="cursive text-blur chat flex flex-row items-center gap-1 hover:underline"
               href="mailto:sarah.emmoth@gmail.com"
             >
               Get in touch! <GoArrowDownLeft />
@@ -16,7 +16,7 @@ const Footer = () => {
           </div>
         </div>
         <div className="w-full">
-          <p className="bottom-2 m-0 absolute">
+          <p className="bottom-2 m-0 absolute text-blur">
             © 2023 Sarah Emmoth - <span className="mono"> Crafted with ❤️</span>
           </p>
         </div>
